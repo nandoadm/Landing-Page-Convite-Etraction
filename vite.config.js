90e9012e10109ac98a7a3f1ccbb92c67271a2fe4
Fernando-Etraction <fernando.rodrigues@etraction.com.br>
2026-05-11 08:55:17 -0300

    FixErros

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Landing-Page-Convite-Etraction/",
+  base: "/convite-especial/",
   build: {
     outDir: "docs"
   }
